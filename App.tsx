01a5a355568f54110b547f0deb34a1cbfd03b623
Quentin Rault <q.rault054@gmail.com>
2022-09-28 14:53:42 +0200

    initialize navigation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { StyleSheet } from 'react-native';
+import HomeScreen from './screens/HomeScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { Icon } from '@expo/vector-icons/build/createIconSet';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>We are the best</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Login') {
+              iconName = focused ? 'log-in' : 'log-in-outline';
+            } else if (route.name === 'Projects') {
+              iconName = focused ? 'camera' : 'camera-outline';
+            } else if (route.name === 'Tickets') {
+              iconName = focused ? 'camera' : 'camera-outline';
+            }
+            return <Ionicons
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+            name={iconName} size={size} color={color} />;
+          },
+          tabBarStyle: { position: 'absolute' },
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet, Button } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+      <Text>Home
+      </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
+
+export default HomeScreen
